9c48a639208bb98b0932b4c0f184fb06692802e4
Jon <sanmijon@mac.com>
2015-05-02 10:59:56 -0400

    modify treestarter

--- TreeStarter.cs
+++ TreeStarter.cs
@@ -3,24 +3,31 @@ using System.Collections;
 
 public class TreeStarter : MonoBehaviour {
 	// Use this for initialization
-	TreeGenerator tg;
-	TreeLeaves tl;
+	public GameObject tree;
+	TreeGenerator [] tg = new TreeGenerator [2];
+	TreeLeaves [] tl = new TreeLeaves [2];
 	void Start () {
-		tg = gameObject.GetComponent <TreeGenerator> () as TreeGenerator;
-		tl = gameObject.GetComponent <TreeLeaves> () as TreeLeaves;
+		GameObject t = Instantiate (tree, Vector3.zero, Quaternion.identity) as GameObject;
+		GameObject t2 = Instantiate (tree, Vector3.left * 10, Quaternion.identity) as GameObject;
+		tg [0] = t.GetComponent <TreeGenerator> () as TreeGenerator;
+		tl [0] = t.GetComponent <TreeLeaves> () as TreeLeaves;
+		tg [1] = t2.GetComponent <TreeGenerator> () as TreeGenerator;
+		tl [1] = t.GetComponent <TreeLeaves> () as TreeLeaves;
 	}
 
 	void Update () {
 		if (Input.GetKeyDown ("up")) {
-			tg.Init ();
-			tg.pEvent += MakeLeaves;
-			StartCoroutine(tg.Grow ());
+			for (int i = 0; i < tg.Length; i++) {
+				tg [i].Init ();
+				if (i == 0) tg [i].pEvent += MakeLeaves;
+				StartCoroutine(tg [i].Grow ());
+			}
 		}
 	}
 
-	void MakeLeaves (TreeGenerator tg) {
+	void MakeLeaves (TreeGenerator tgi) {
 		Debug.Log ("done");
-		tl.tg = tg;
-		tl.MakeLikeATree ();
+		tl [0].tg = tg [0];
+		tl [0].MakeLikeATree ();
 	}
 }
